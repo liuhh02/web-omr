65d6cdc9b8c591cdee7794728abb38224a55f239
justinsmits <justin.r.smits@gmail.com>
2022-04-21 06:06:28 -0500

    Creating a docker container to simplify developer environment configuration and updating to newer version of tensorflow

--- app.py
+++ app.py
@@ -4,9 +4,13 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 import cv2
 import numpy as np
+
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
+
+tf.compat.v1.disable_eager_execution()
+
 app = Flask(__name__, static_url_path='')
 
 def sparse_tensor_to_strs(sparse_tensor):
@@ -15,7 +19,7 @@ def sparse_tensor_to_strs(sparse_tensor):
   dense_shape = sparse_tensor[0][2]
 
   strs = [ [] for i in range(dense_shape[0]) ]
-
+  
   string = []
   ptr = 0
   b = 0
@@ -47,8 +51,8 @@ def resize(image, height):
 voc_file = "vocabulary_semantic.txt"
 model = "Semantic-Model/semantic_model.meta"
 
-tf.reset_default_graph()
-sess = tf.InteractiveSession()
+#tf.reset_default_graph()
+sess = tf.compat.v1.InteractiveSession()
 # Read the dictionary
 dict_file = open(voc_file,'r')
 dict_list = dict_file.read().splitlines()
@@ -59,17 +63,17 @@ for word in dict_list:
 dict_file.close()
 
 # Restore weights
-saver = tf.train.import_meta_graph(model)
+saver = tf.compat.v1.train.import_meta_graph(model)
 saver.restore(sess,model[:-5])
 
-graph = tf.get_default_graph()
+graph = tf.compat.v1.get_default_graph()
 
 input = graph.get_tensor_by_name("model_input:0")
 seq_len = graph.get_tensor_by_name("seq_lengths:0")
 rnn_keep_prob = graph.get_tensor_by_name("keep_prob:0")
 height_tensor = graph.get_tensor_by_name("input_height:0")
 width_reduction_tensor = graph.get_tensor_by_name("width_reduction:0")
-logits = tf.get_collection("logits")[0]
+logits = tf.compat.v1.get_collection("logits")[0]
 
 # Constants that are saved inside the model itself
 WIDTH_REDUCTION, HEIGHT = sess.run([width_reduction_tensor, height_tensor])
